7c65f75a60b016a708c282416a1fbe65632fb55a
Asmit03 <asmitchaudharyyy03@gmail.com>
2026-08-21 17:29:48 +0530

    Initialize project and FastAPI backend

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Voice Analytics Assistant API",
+    version="1.0.0"
+)
+
+@app.get("/api/health")
+def health_check():
+    return {
+        "status": "ok"
+    }
